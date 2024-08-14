2903c584ea291c7351e305742a2144adc0476c3c
gaopeiyuan <121370451@qq.com>
2024-08-14 23:51:44 +0800

    🕰️ replace previous baseline2 with naive time enumeration method

--- Historical/graph_v_of_v/graph_v_of_v_with_time_span.h
+++ Historical/graph_v_of_v/graph_v_of_v_with_time_span.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <vector>
-#include "Historical/tool_functions/sorted_vector_binary_operations.h"
+#include "CPU/tool_functions/sorted_vector_binary_operations.h"
 #include "CPU/graph_v_of_v/graph_v_of_v.h"
 #include <boost/heap/fibonacci_heap.hpp>
 
@@ -16,60 +16,24 @@ public:
 	int startTimeLabel;
 	int endTimeLabel;
 	EdgeInfo() : vertex(-1) {}
-	EdgeInfo(int vertex, weight_type weight, int startTimeLabel, int endTimeLabel)
-		: vertex(vertex), weight(weight), startTimeLabel(startTimeLabel), endTimeLabel(endTimeLabel) {}
-	bool operator>(int v_id) const
+	EdgeInfo(int vertex, weight_type weight, int startTimeLabel)
+		: vertex(vertex), weight(weight), startTimeLabel(startTimeLabel)
 	{
-		return vertex > v_id;
-	}
-	bool operator==(int v_id) const
-	{
-		return vertex == v_id;
+		endTimeLabel = INT_MAX;
 	}
 };
-
-template <typename weight_type> // weight_type may be int, long long int, float, double...
-class NodeWithTimeSpan
-{
-public:
-	int vertex;
-	weight_type weight;
-	int startTimeLabel;
-	int endTimeLabel;
-	NodeWithTimeSpan(int vertex, weight_type weight, int startTimeLabel, int endTimeLabel)
-		: vertex(vertex), weight(weight), startTimeLabel(startTimeLabel), endTimeLabel(endTimeLabel) {}
-	NodeWithTimeSpan(int vertex, weight_type weight) : vertex(vertex), weight(weight) {}
-	bool operator>(weight_type w) const
-	{
-		return weight > w;
-	}
-	bool operator==(weight_type w) const
-	{
-		return weight == w;
-	}
-	bool operator<(weight_type w) const
-	{
-		return weight < w;
-	}
-};
-
-struct compare_node
-{
-	template <typename weight_type>
-	bool operator()(const NodeWithTimeSpan<weight_type> &lhs, const NodeWithTimeSpan<weight_type> &rhs) const
-	{
-		return lhs.weight > rhs.weight;
-	}
-};
-
+template <typename weight_type>
 struct compare_pair
 {
-	bool operator()(const pair<int, int> &lhs, const pair<int, int> &rhs) const
+	bool operator()(const pair<int, weight_type> &lhs, const pair<int, weight_type> &rhs) const
 	{
 		return lhs.second > rhs.second;
 	}
 };
 
+/**
+ * define a graph where each edge has an associated time span
+ */
 template <typename weight_type> // weight_type may be int, long long int, float, double...
 class graph_v_of_v_with_time_span
 {
@@ -79,13 +43,20 @@ public:
 
 	this class is for undirected and edge-weighted-time-span-label graph
 	*/
-	std::vector<std::vector<EdgeInfo<weight_type> *>> ADJs;
-	std::vector<std::vector<EdgeInfo<weight_type> *>> preLabels;
+	// i - 0,j - 0->pair->first=1,second = vector:{<1,23,0,0> <1,30,1,1> <1,11,2,3> <1,8,4,6> <1,17,7,2147483647>}
+	vector<vector<pair<int, vector<EdgeInfo<weight_type>>>>> ADJs;
+
 	/*constructors*/
 	graph_v_of_v_with_time_span() {}
-	graph_v_of_v_with_time_span(int n) : ADJs(n), preLabels(n, std::vector<EdgeInfo<weight_type> *>(n, nullptr))
+	graph_v_of_v_with_time_span(int n, int e, weight_type weight_upper_limit, weight_type weight_lower_limit) : v_num(n), e_num(e), weight_dis(weight_lower_limit, weight_upper_limit), ADJs(n)
 	{
-		ADJs.resize(n); // initialize n vertices
+		if (weight_lower_limit > weight_upper_limit || weight_lower_limit < 0 || weight_upper_limit < 0)
+		{
+			cout << "weight_lower_limit should be greater than 0 and weight_upper_limit shoule be greater 0 and the weight_lower_limit should be less than weight_upper_limit"
+				 << "weight_upper_limit is :" << weight_upper_limit
+				 << "weight_lower_limit is :" << weight_lower_limit << endl;
+			exit(1);
+		}
 	}
 	int size()
 	{
@@ -95,82 +66,177 @@ public:
 	{
 		return ADJs[i];
 	}
-	~graph_v_of_v_with_time_span()
-	{
-		for (auto &adjList : ADJs)
-		{
-			for (auto edge : adjList)
-			{
-				delete edge;
-			}
-		}
-	}
 	/*class member functions*/
-	inline void add_edge(int, int, weight_type, int);
-	inline void add_graph_time(graph_v_of_v<weight_type>, int);
-	inline int search_shortest_path_in_period_time(int, int, int, int);
+	inline weight_type search_shortest_path_in_period_time_naive(int, int, int, int);
+
 	inline void print();
-	// inline int dijkstra_iterator(vector<graph_v_of_v<weight_type>>, int, int);
+	inline vector<graph_v_of_v<weight_type>> graph_v_of_v_generate_random_graph_with_same_edges_of_different_weight(int change_num, int maintain_percent);
+
+private:
+	/* the maximum of time*/
+	int time_max;
+	/* the number of vertices */
+	int v_num;
+	/* the number of edges */
+	int e_num;
+	/* weighted random number generator */
+	uniform_int_distribution<weight_type> weight_dis;
+
+	inline void add_edge(int, int, weight_type, int);
+
+	inline void add_graph_time(graph_v_of_v<weight_type>, int);
 };
 
 /*class member functions*/
+/**
+ * param
+ * 	@u is the source vertex,
+ *  @v is the target vertex,
+ * 	@startTime,
+ * 	@endTime
+ */
+template <typename weight_type>
+weight_type graph_v_of_v_with_time_span<weight_type>::search_shortest_path_in_period_time_naive(int u, int v, int startTime, int endTime)
+{
+	double res = __DBL_MAX__;
+	int N = this->v_num;
+	std::vector<double> dist(N);
+	std::vector<bool> visited(N);
+	boost::heap::fibonacci_heap<pair<int, double>, boost::heap::compare<compare_pair<weight_type>>> queue;
+	for (int queryTime = startTime; queryTime <= endTime; queryTime++)
+	{
+		dist.assign(N, __DBL_MAX__);
+		visited.assign(N, false);
+		queue.clear();
+		dist[u] = 0;
+		queue.push({u, 0});
+		while (queue.size() > 0)
+		{
+			int vertexBase = queue.top().first;
+			queue.pop();
+			if (vertexBase == v)
+			{
+				res = min(res, dist[vertexBase]);
+			}
+			if (visited[vertexBase])
+				continue;
+			visited[vertexBase] = true;
+
+			for (const auto &vertices : this->ADJs[vertexBase])
+			{
+				int next = vertices.first;
+				for (const auto &edge_info_time_span : vertices.second)
+				{
+					if (edge_info_time_span.startTimeLabel <= queryTime && edge_info_time_span.endTimeLabel >= queryTime)
+					{
+						if (dist[vertexBase] + edge_info_time_span.weight < dist[next])
+						{
+							dist[next] = dist[vertexBase] + edge_info_time_span.weight;
+							queue.push({next, dist[next]});
+						}
+						break;
+					}
+				}
+			}
+		}
+		// cout << "naive" << res << endl;
+	}
+	return res == __DBL_MAX__ ? -1 : res;
+}
+
+template <typename weight_type>
+void graph_v_of_v_with_time_span<weight_type>::print()
+{
+	std::cout << "graph_v_of_v_with_time_span_print:" << std::endl;
+	int size = this->ADJs.size();
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "Vertex " << i << " Adj List: ";
+		for (const auto &edges : ADJs[i])
+		{
+			int v_id = edges.first;
+			for (const auto &info : edges.second)
+			{
+				std::cout << "<" << v_id << "," << info.weight << "," << info.startTimeLabel << "," << info.endTimeLabel << "> ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "graph_v_of_v_with_time_span_print END" << std::endl;
+}
+
+template <typename weight_type>
+vector<graph_v_of_v<weight_type>> graph_v_of_v_with_time_span<weight_type>::graph_v_of_v_generate_random_graph_with_same_edges_of_different_weight(int change_num, int maintain_percent)
+{
+	if (maintain_percent > 10 || maintain_percent < 0)
+	{
+		cout << "maintain_percent should be between 0 and 10: " << maintain_percent << endl;
+		exit(1);
+	}
+	if (change_num < 0)
+	{
+		cout << "the time_max should be greater than or equal to 0" << endl;
+	}
+	this->time_max = change_num;
+	boost::random::mt19937 boost_random_time_seed{static_cast<std::uint32_t>(std::time(0))};
+	graph_v_of_v<weight_type> instance_graph;
+	instance_graph = graph_v_of_v_generate_random_graph<weight_type>(this->v_num, this->e_num, this->weight_dis.min(), this->weight_dis.max(), 1, boost_random_time_seed);
+	add_graph_time(instance_graph, 0);
+	vector<graph_v_of_v<weight_type>> res;
+	res.push_back(instance_graph);
+	uniform_int_distribution<> dis(1, 10);
+	int index = 1;
+	int N = instance_graph.ADJs.size();
+	while (index <= change_num)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			auto &vertices = instance_graph.ADJs[i];
+			for (auto &edges : vertices)
+			{
+				if (edges.first > i && dis(boost_random_time_seed) > maintain_percent)
+				{
+					edges.second = weight_dis(boost_random_time_seed);
+					int to_position = sorted_vector_binary_operations_search_position(instance_graph.ADJs[edges.first], i);
+					instance_graph.ADJs[edges.first][to_position].second = edges.second;
+					add_edge(i, edges.first, edges.second, index);
+					add_edge(edges.first, i, edges.second, index);
+				}
+			}
+		}
+		res.push_back(instance_graph);
+		++index;
+	}
+	return res;
+}
 
 template <typename weight_type>
 void graph_v_of_v_with_time_span<weight_type>::add_edge(int e1, int e2, weight_type ec, int time)
 {
-	if (preLabels[e1][e2] != nullptr && preLabels[e1][e2]->endTimeLabel == time - 1 && preLabels[e1][e2]->weight == ec)
+	/* initialize a graph with a time span */
+	if (time == 0)
 	{
-		preLabels[e1][e2]->endTimeLabel = time;
-		preLabels[e2][e1]->endTimeLabel = time;
+		this->ADJs[e1].push_back({e2, {EdgeInfo(e2, ec, time)}});
+		this->ADJs[e2].push_back({e1, {EdgeInfo(e1, ec, time)}});
 	}
 	else
 	{
-		EdgeInfo<weight_type> *edgeInfoE1 = new EdgeInfo<weight_type>(e2, ec, time, time);
-		this->ADJs[e1].push_back(edgeInfoE1);
-		EdgeInfo<weight_type> *edgeInfoE2 = new EdgeInfo<weight_type>(e1, ec, time, time);
-		this->ADJs[e2].push_back(edgeInfoE2);
-		preLabels[e1][e2] = edgeInfoE1;
-		preLabels[e2][e1] = edgeInfoE2;
+		int index_e1 = sorted_vector_binary_operations_search_position<vector<EdgeInfo<weight_type>>>(this->ADJs[e1], e2);
+		int index_e2 = sorted_vector_binary_operations_search_position<vector<EdgeInfo<weight_type>>>(this->ADJs[e2], e1);
+		if (this->ADJs[e1][index_e1].second.back().weight != ec)
+		{
+			this->ADJs[e1][index_e1].second.back().endTimeLabel = time - 1;
+			this->ADJs[e2][index_e2].second.back().endTimeLabel = time - 1;
+			this->ADJs[e1][index_e1].second.push_back(EdgeInfo(e2, ec, time));
+			this->ADJs[e2][index_e2].second.push_back(EdgeInfo(e1, ec, time));
+		}
 	}
-	// if (this->ADJs[e1].size() != index && this->ADJs[e1][index].vertex == e2 && this->ADJs[e1][index].weight == ec && this->ADJs[e1][index].endTimeLabel == time - 1)
-	// {
-	// 	this->ADJs[e1][index].endTimeLabel = time;
-	// }
-	// else if (this->ADJs[e1].size() == index || this->ADJs[e1][index].vertex != e2)
-	// {
-	// 	EdgeInfo<weight_type> edgeInfo(e2, ec, time, time);
-	// 	this->ADJs[e1].insert(ADJs[e1].begin() + index, edgeInfo);
-	// }
-	// else
-	// {
-	// 	EdgeInfo<weight_type> edgeInfo(e2, ec, time, time);
-	// 	this->ADJs[e1].insert(this->ADJs[e1].begin() + index + 1, edgeInfo);
-	// }
-	// index = sorted_vector_binary_operations(this->ADJs[e2], e1);
-	// if (this->ADJs[e2].size() != index && this->ADJs[e2][index].vertex == e1 && this->ADJs[e2][index].weight == ec && this->ADJs[e2][index].endTimeLabel == time - 1)
-	// {
-	// 	this->ADJs[e2][index].endTimeLabel = time;
-	// }
-	// else if (this->ADJs[e2].size() == index || this->ADJs[e2][index].vertex != e1)
-	// {
-	// 	EdgeInfo<weight_type> edgeInfo(e1, ec, time, time);
-	// 	this->ADJs[e2].insert(ADJs[e2].begin() + index, edgeInfo);
-	// }
-	// else
-	// {
-	// 	EdgeInfo<weight_type> edgeInfo(e1, ec, time, time);
-	// 	this->ADJs[e2].insert(ADJs[e2].begin() + index + 1, edgeInfo);
-	// }
 }
 
 template <typename weight_type>
 void graph_v_of_v_with_time_span<weight_type>::add_graph_time(graph_v_of_v<weight_type> graph, int time)
 {
 	int N = graph.size();
-	if (N > this->ADJs.size())
-	{
-		this->ADJs.resize(N);
-	}
 	for (int i = 0; i < N; i++)
 	{
 		std::vector<std::pair<int, weight_type>> list = graph.ADJs[i];
@@ -185,133 +251,12 @@ void graph_v_of_v_with_time_span<weight_type>::add_graph_time(graph_v_of_v<weigh
 	}
 }
 
-/**
- *
- *
- */
-/**
- * param
- * 	@u is the source vertex,
- *  @v is the target vertex,
- * 	@startTime,
- * 	@endTime
- */
 template <typename weight_type>
-int graph_v_of_v_with_time_span<weight_type>::search_shortest_path_in_period_time(int u, int v, int startTime, int endTime)
+weight_type dijkstra(graph_v_of_v<weight_type> graph, int u, int v)
 {
-	if (v > ADJs.size() || u > ADJs.size() || u < 0 || v < 0)
-	{
-		std::cout << "u and v shoule be between 0 and N" << endl;
-		return -1;
-	}
-	int N = this->ADJs.size();
-	/**
-	 * i -> vertex
-	 * j -> index of time_span
-	 * pair->time_span
-	 */
-	vector<vector<pair<int, int>>> visited(N);
-	/* a class contains information about destination vertex, hop count, and cost in the priority queue */
-	boost::heap::fibonacci_heap<NodeWithTimeSpan<weight_type>, boost::heap::compare<compare_node>> queue;
-	NodeWithTimeSpan node(u, 0, startTime, endTime);
-	queue.push(node);
-	int res = -1;
-
-	while (queue.size() > 0)
-	{
-		node = queue.top();
-		queue.pop();
-		if (node.vertex == v)
-		{
-			return node.weight;
-		}
-		int vertexBase = node.vertex;
-		int curWeight = node.weight;
-		int startTimeLabel = node.startTimeLabel;
-		int endTimeLabel = node.endTimeLabel;
-		/* the edges with uncalculated results */
-		vector<pair<int, int>> temp;
-		vector<pair<int, int>> iteraotr_items;
-		int pre = startTimeLabel, after = endTimeLabel;
-		int index = startTimeLabel;
-		int i = 0;
-		int len = visited[vertexBase].size();
-
-		while (i < len && visited[vertexBase][i].second < startTimeLabel)
-		{
-			temp.push_back(visited[vertexBase][i]);
-			++i;
-		}
-		if (i < len && startTimeLabel >= visited[vertexBase][i].first && endTimeLabel <= visited[vertexBase][i].second)
-		{
-			continue;
-		}
-		while (i < len && visited[vertexBase][i].first <= endTimeLabel)
-		{
-			if (index < visited[vertexBase][i].first)
-			{
-				iteraotr_items.push_back({index, visited[vertexBase][i].first - 1});
-			}
-			index = visited[vertexBase][i].second + 1;
-			pre = min(pre, visited[vertexBase][i].first);
-			after = max(after, visited[vertexBase][i].second);
-			++i;
-		}
-		if (i == len && index <= endTimeLabel)
-		{
-			iteraotr_items.push_back({index, endTimeLabel});
-		}
-		else if (i < len)
-		{
-			iteraotr_items.push_back({index, visited[vertexBase][i].first});
-		}
-		temp.push_back({pre, after});
-		while (i < len)
-		{
-			temp.push_back(visited[vertexBase][i]);
-			i++;
-		}
-		visited[vertexBase] = temp;
-		for (const auto &edges : this->ADJs[vertexBase])
-		{
-			for (const auto &dfs_time_span : iteraotr_items)
-			{
-				if (!(edges->startTimeLabel > dfs_time_span.second || edges->endTimeLabel < dfs_time_span.first))
-				{
-					node.vertex = edges->vertex;
-					node.weight = curWeight + edges->weight;
-					node.startTimeLabel = max(edges->startTimeLabel, dfs_time_span.first);
-					node.endTimeLabel = min(edges->endTimeLabel, dfs_time_span.second);
-					queue.push(node);
-				}
-			}
-		}
-	}
-	return res;
-}
-template <typename weight_type>
-void graph_v_of_v_with_time_span<weight_type>::print()
-{
-	std::cout << "graph_v_of_v_with_time_span_print:" << std::endl;
-	int size = this->ADJs.size();
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << "Vertex " << i << " Adj List: ";
-		int v_size = ADJs[i].size();
-		for (int j = 0; j < v_size; j++)
-		{
-			std::cout << "<" << ADJs[i][j]->vertex << "," << ADJs[i][j]->weight << "," << ADJs[i][j]->startTimeLabel << "," << ADJs[i][j]->endTimeLabel << "> ";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "graph_v_of_v_with_time_span_print END" << std::endl;
-}
-
-int dijkstra(graph_v_of_v<int> graph, int u, int v)
-{
-	std::vector<int> dist(graph.size(), INT_MAX);
+	std::vector<double> dist(graph.size(), __DBL_MAX__);
 	std::vector<bool> visited(graph.size(), false);
-	boost::heap::fibonacci_heap<pair<int, int>, boost::heap::compare<compare_pair>> queue;
+	boost::heap::fibonacci_heap<pair<int, weight_type>, boost::heap::compare<compare_pair<weight_type>>> queue;
 	dist[u] = 0;
 	queue.push({u, 0});
 	while (queue.size() > 0)
@@ -341,12 +286,14 @@ int dijkstra(graph_v_of_v<int> graph, int u, int v)
 	return -1;
 }
 
-int dijkstra_iterator(vector<graph_v_of_v<int>> list, int u, int v)
+template <typename weight_type>
+int dijkstra_iterator(vector<graph_v_of_v<weight_type>> list, int u, int v)
 {
 	int res = INT_MAX;
 	for (graph_v_of_v<int> graph : list)
 	{
 		res = min(res, dijkstra(graph, u, v));
+		// cout << "dijkstra" << res << endl;
 	}
 	return res;
 }
--- Historical/test/baseline_test.cpp
+++ Historical/test/baseline_test.cpp
@@ -6,20 +6,16 @@ using namespace std;
 int testBaseLineAndBaseline2()
 {
     // generate a ramdom graph
-    // vector<graph_v_of_v<int>> graphs = generate_in_same_edge_different_weight(5, 10, 10, 30, 6, 6);
-    // graph_v_of_v_with_time_span<int> graphWithTimeSpan(5);
-    vector<graph_v_of_v<int>> graphs = generate_in_same_edge_different_weight(5000, 10000, 10, 30, 6, 6);
-    graph_v_of_v_with_time_span<int> graphWithTimeSpan(5000);
+    // graph_v_of_v_with_time_span<int> graphWithTimeSpan(5, 10, 30, 6);
+    // vector<graph_v_of_v<int>> graphs = graphWithTimeSpan.graph_v_of_v_generate_random_graph_with_same_edges_of_different_weight(10, 6);
+    // generate a larger ramdom graph
+    graph_v_of_v_with_time_span<int> graphWithTimeSpan(5000, 100000, 30, 6);
+    vector<graph_v_of_v<int>> graphs = graphWithTimeSpan.graph_v_of_v_generate_random_graph_with_same_edges_of_different_weight(100, 3);
     // print the graphs
-    // for(graph_v_of_v<int> graph : graphs){
+    // for (graph_v_of_v<int> graph : graphs)
+    // {
     //     graph.print();
     // }
-
-    // generate the time span
-    for (int i = 0; i < graphs.size(); i++)
-    {
-        graphWithTimeSpan.add_graph_time(graphs[i], i);
-    }
     // graphWithTimeSpan.print();
 
     // // dijkstra_iterator baseline 1
@@ -28,37 +24,30 @@ int testBaseLineAndBaseline2()
     int res_n_iterate_dijkstra = dijkstra_iterator(subsequence, 1, 4);
     auto endTime = std::chrono::high_resolution_clock::now();
     double runtime_n_iterate_dijkstra = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - start_time).count() / 1e9;
-    // std::cout << resIterator << std::endl;
+    // std::cout << res_n_iterate_dijkstra << std::endl;
 
     // dfs to calculate the shortest path baseline 2
     auto start_time_base_line_2 = std::chrono::high_resolution_clock::now();
-    int res_base_line_with_span = graphWithTimeSpan.search_shortest_path_in_period_time(1, 4, 3, 7);
+    int res_base_line_with_span = graphWithTimeSpan.search_shortest_path_in_period_time_naive(1, 4, 3, 7);
     auto end_time_base_line_2 = std::chrono::high_resolution_clock::now();
     double runtime_base_line_with_span = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time_base_line_2 - start_time_base_line_2).count() / 1e9;
 
     // // print the result
-    std::cout << res_n_iterate_dijkstra << ":" << res_base_line_with_span << std::endl;
+    std::cout << res_n_iterate_dijkstra  << ":" << res_base_line_with_span << std::endl;
     std::cout << runtime_n_iterate_dijkstra << ":" << runtime_base_line_with_span << std::endl;
     // debug
-    graphWithTimeSpan.print();
-    for (graph_v_of_v<int> graph : graphs)
-    {
-        graph.print();
-    }
+    // graphWithTimeSpan.print();
+    // for (graph_v_of_v<int> graph : graphs)
+    // {
+    //     graph.print();
+    // }
 
-    // // dijkstra_iterator baseline 1
-    auto start_time_test = std::chrono::high_resolution_clock::now();
-    int res_n_iterate_dijkstra_test = dijkstra_iterator(subsequence, 1, 4);
-    auto endTime_test = std::chrono::high_resolution_clock::now();
-
-    // dfs to calculate the shortest path baseline 2
-    auto start_time_base_line_2_test = std::chrono::high_resolution_clock::now();
-    int res_base_line_with_span_test = graphWithTimeSpan.search_shortest_path_in_period_time(1, 4, 3, 7);
-    auto end_time_base_line_2_test = std::chrono::high_resolution_clock::now();
-
+    // // // dijkstra_iterator baseline 1
+    // int res_base_line_with_span_debug = graphWithTimeSpan.search_shortest_path_in_period_time_naive(1, 4, 3, 7);
     return 0;
 }
 int main()
 {
     testBaseLineAndBaseline2();
+    exit(0);
 }
