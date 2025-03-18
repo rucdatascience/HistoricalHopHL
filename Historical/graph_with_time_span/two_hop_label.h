aa0f26f17ec01ff03fd108fbb58bf1ac8c44b76c
gaopeiyuan <121370451@qq.com>
2025-03-18 21:35:12 +0800

    fix:1

--- Historical/graph_with_time_span/two_hop_label.h
+++ Historical/graph_with_time_span/two_hop_label.h
@@ -1468,17 +1468,17 @@ namespace experiment
 
 				outputFile << "compute_label_bit_size()=" << compute_label_bit_size() << std::endl;
 
-				int index = 0;
-				std::cout << "print_L: (hub_vertex, hop, distance)" << std::endl;
-				for (auto &xx : L)
-				{
-					outputFile << "vertex " << index++ << ": ";
-					for (auto &yy : xx)
-					{
-						outputFile << "(" << yy.hub_vertex << "," << yy.hop << "," << yy.distance << "," << yy.t_s << "," << yy.t_e << ")";
-					}
-					outputFile << std::endl;
-				}
+				// int index = 0;
+				// std::cout << "print_L: (hub_vertex, hop, distance)" << std::endl;
+				// for (auto &xx : L)
+				// {
+				// 	outputFile << "vertex " << index++ << ": ";
+				// 	for (auto &yy : xx)
+				// 	{
+				// 		outputFile << "(" << yy.hub_vertex << "," << yy.hop << "," << yy.distance << "," << yy.t_s << "," << yy.t_e << ")";
+				// 	}
+				// 	outputFile << std::endl;
+				// }
 			}
 
 			long long int query(int source, int terminal, int t_s, int t_e, int hop_cst)
