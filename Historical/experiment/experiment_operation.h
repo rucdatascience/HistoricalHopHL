50defa1abe503d6887311020ced2ab1be950077d
gaopeiyuan <121370451@qq.com>
2025-03-19 17:56:54 +0800

    fix:1

--- Historical/experiment/experiment_operation.h
+++ Historical/experiment/experiment_operation.h
@@ -931,7 +931,7 @@ namespace experiment
 			timer.endSubtask();
 			//----------------------------------------------- step 4: canonical_repair---------------------------------------------------------------
 			timer.startSubtask("step 4: canonical_repair");
-			// hop_constrained_clean_L(case_info, num_of_threads);
+			hop_constrained_clean_L(case_info, num_of_threads);
 			timer.endSubtask();
 			//---------------------------------------------------------------------------------------------------------------------------------------
 			hop_constrained_clear_global_values<weight_type>();
